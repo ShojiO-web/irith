f15e361392df9a35f0a7f5e6d15a9846789ed130
irith <118494231+ShojiO-web@users.noreply.github.com>
2022-11-18 10:57:36 +0900

    Create visual.js

---
+++ freemarket/visual.js
@@ -0,0 +1,54 @@
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    speed: 500,//スライドのスピード。初期値は300。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    centerMode: true,//要素を中央ぞろえにする
+    variableWidth: true,//幅の違う画像の高さを揃えて表示
+    dots: true,//下部ドットナビゲーションの表示
+});
+
+//アコーディオンをクリックした時の動作
+$('.title').on('click', function() {//タイトル要素をクリックしたら
+	var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+	$(findElm).slideToggle();//アコーディオンの上下動作
+    
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去し
+	}else{//それ以外は
+		$(this).addClass('close');//クラス名closeを付与
+	}
+});
+
+//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+$(window).on('load', function(){
+	$('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+	$(".open").each(function(index, element){	//openクラスを取得
+		var Title =$(element).children('.title');	//openクラスの子要素のtitleクラスを取得
+		$(Title).addClass('close');				//タイトルにクラス名closeを付与し
+		var Box =$(element).children('.box');	//openクラスの子要素boxクラスを取得
+		$(Box).slideDown(500);					//アコーディオンを開く
+	});
+});
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $(".circle-bg").toggleClass('circleactive');//丸背景にcircleactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去
+    $(".circle-bg").removeClass('circleactive');//丸背景のcircleactiveクラスを除去
+});
+
+$('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
